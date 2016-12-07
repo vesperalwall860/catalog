33c67826eb587f97f6805434e4904ab98fda587a
Dmitriy Butovskiy <vesperalwall860@gmail.com>
2016-12-07 03:36:11 +0200

    Added fields for the product model

--- project/models.py
+++ project/models.py
@@ -1,1 +1,8 @@
 from . import db
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    price = db.Column(db.Float)
+    url = db.Column(db.String)
+    images = db.Column(db.Text)
