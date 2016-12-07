4a6612ec2c97509ffba1ea4dfbd625c914f897d3
Dmitriy Butovskiy <vesperalwall860@gmail.com>
2016-12-07 03:20:59 +0200

    Edited the __init__.py file

--- project/__init__.py
+++ project/__init__.py
@@ -1,4 +1,37 @@
 from flask import Flask
-app = Flask(__name__)
+from flask import session, g
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_login import current_user
+from .config import config
+from flask_mail import Mail
+from datetime import timedelta
 
-import project.views
+db = SQLAlchemy()
+mail = Mail()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
+    # Auto Time Out After 60 Minutes For Session
+    @app.before_request
+    def before_request():
+        session.permanent = True
+        app.permanent_session_lifetime = timedelta(minutes=60)
+        session.modified = True
+        g.user = current_user
+
+    db.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    from . import views, models
+    return app
